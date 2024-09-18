915a519beea7f834c2777be38a5b8376e1ecbfdd
PICCI-RIDOY <107355397+RIDOY-404-CYBER@users.noreply.github.com>
2024-09-18 11:39:07 +0600

    Update AWR.py

--- AWR.py
+++ AWR.py
@@ -2,7 +2,7 @@ import os,platform
 os.system("git pull")
 rmx=platform.architecture()[0]
 if rmx=='64bit':
-  import CYR
+  import CR
 if rmx=='32bit':
-  import CYR2
+  import CR2
 else:print('\033[1;32m[•] Contract Admin For Help');exit()
